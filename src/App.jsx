1960611e7b6203d651187ce63a59f2b18c3bf2c9
Mohit Sharma <mohit29code@gmail.com>
2024-01-23 05:17:41 +0530

    Optimze1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import './App.css';
 import Card from './components/Card.jsx';
 import Loader from './components/Loader';
@@ -8,15 +8,18 @@ function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
-
+  const initialRender = useRef(true);
+  var lock=0;
  
   //Fetch most starred repos using Github Rest API
   const fetchData = async () => {
+   
     try {
       setLoading(true);
       const date=reqDate();
       console.log(date)
       const url=`https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc&page=${page}`
+      
       const res = await fetch(url);
       const repoData = await res.json();
       console.log(repoData)
@@ -26,7 +29,9 @@ function App() {
         return;
       }
       console.log(repos)
+  
       setData((prevData) => [...prevData, ...repos]);
+
       setLoading(false);
     } catch (err) {
       setLoading(false);
@@ -37,14 +42,26 @@ function App() {
   //Check for infinite scroll condition
   const controlScroll = () => {
     const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
-    if (clientHeight + scrollTop >= scrollHeight - 10 && !loading) {
+   
+    if (clientHeight + scrollTop >= scrollHeight - 10 && !loading ) {
       setLoading(true);
+      console.log(loading)
+      console.log(lock)
+      if(lock==0)  
       setPage((prev) => prev + 1);
+      setTimeout(()=>{
+        lock=0;
+      },2000)
+       lock=1;
     }
   };
 
   useEffect(() => {
-    fetchData();
+    if (!initialRender.current) {
+      fetchData();
+    } else {
+      initialRender.current = false;
+    }
   }, [page]);
 
   useEffect(() => {
